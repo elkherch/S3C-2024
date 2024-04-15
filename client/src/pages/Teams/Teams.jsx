49ab42715587482dff63d84cf63b621356314f56
CHiekhA <cheikhm@el-technology.com>
2024-04-15 08:50:12 +0000

    hello

--- client/src/pages/Teams/Teams.jsx
+++ client/src/pages/Teams/Teams.jsx
@@ -1,3 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Table from '../../components/table/Table';
 import {fetchTeams  } from '../../services/Teamservices/Teamservices';
+
+const Teams = () => {
+   
+    return (
+        <>
+        </>
+    );
+};
+export default Teams;
