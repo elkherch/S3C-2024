b160054623f078522b753a4ec93b110a8cdbd9ca
Begnoug <21024@supnum.mr>
2024-04-15 15:29:40 +0000

    Integration de l'authentification des differents utilisateurs du système

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,15 +3,15 @@ const authService = require('../services/AuthServices');
 
 async function Login(req, res) {
     try {
-        const user = await userService.authenticate(req.body.username, req.body.password);
+        const user = await userService.authenticate(req.body.email, req.body.password);
 
     if (!user) {
         return res.status(401).json({ message: 'Nom d\'utilisateur ou mot de passe incorrect' });
     }
     const token = authService.generateToken(user);
-    res.json({ token });
+    return res.json({ token , userID: req.body.email});
     } catch (error) {
-        
+        return res.status(500).json({message: error})
     }
 };
 
--- server/services/userService.js
+++ server/services/userService.js
@@ -1,9 +1,13 @@
 const Users = require('../model/UserModels');
 const bcrypt = require('bcrypt');
+const {hashSync} = require("bcrypt");
+
 
 class UserService {
     async createUser(userData) {
         try {
+            const pass = userData.password
+            userData.password = hashSync(pass, 10)
             const user = await Users.create(userData);
             return user;
         } catch (error) {
@@ -76,13 +80,12 @@ class UserService {
     }
     
 
-    async authenticate(username, password) {
+    async authenticate(email, password) {
         try {
-            const user = await Users.findOne({ where: { username: username } });
+            const user = await Users.findOne({ email: email });
             if (!user) {
                 throw new Error('Nom d\'utilisateur ou mot de passe incorrect');
             }
-
             const isPasswordValid = await bcrypt.compare(password, user.password);
             if (!isPasswordValid) {
                 throw new Error('Nom d\'utilisateur ou mot de passe incorrect');
